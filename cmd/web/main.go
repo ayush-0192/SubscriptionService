733c20e150510bd2106c1450c6f64839fde68190
Ayush <ayushtpr@gmail.com>
2026-05-16 00:57:26 +0530

    add go.mod and main.go file

---
+++ cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// connect to database
+
+	// create session
+
+	// create channels
+
+	// create waitgroups
+
+	// set up application config
+
+	// set up mail
+
+	//listen for web connection
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module SubscriptionService
+
+go 1.25.6
